c713976af1061f51c24ee0573b65a5e93ceea2c7
ergeshev <eergeshev@gmail.com>
2020-04-26 05:15:28 +0600

    python version

--- myblog/settings.py
+++ myblog/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!+v^u7w&c594aqkpa9)@^8m)vhw#4po8l5n34*_*zw)hs8kvus'
+# SECRET_KEY = '!+v^u7w&c594aqkpa9)@^8m)vhw#4po8l5n34*_*zw)hs8kvus'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!+v^u7w&c594aqkpa9)@^8m)vhw#4po8l5n34*_*zw)hs8kvus')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', ' ') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +125,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
