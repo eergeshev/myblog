43e76fc2efc17c1453c7d58bfeff11a380061b05
ergeshev <eergeshev@gmail.com>
2020-04-27 04:59:51 +0600

    static settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -134,3 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
