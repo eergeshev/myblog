ab9b405eec5bb4cdafe2da55e798b05953694e41
ergeshev <eergeshev@gmail.com>
2020-04-27 05:57:00 +0600

    static dir fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -134,7 +134,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
